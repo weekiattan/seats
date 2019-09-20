f352f2dbe3d7bb9ca1aaa2a32aba04f955134534
TanWeeKiat <tanwee.kiat@live.com.sg>
2019-09-20 15:03:04 +0800

    part 1 done !

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-console.log("hello script js");
+
+
+var seat = 10;
+var firstPrice = 50;
+var currentPrice;
+
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
-};
+    if(seat === 10){ //find the price of the first seat
+       seat--;
+   currentPrice = firstPrice;
+      return `$ ${currentPrice} ${seat} seats left`
+      // '$ ' + currentPrice + ' '+seat + " seats left";
+    }if(seat === 0){
+        return `No more seats left`;
+    }else {
+        seat --;
+        currentPrice = ((currentPrice * 0.05) + currentPrice);
+        return `$ ${currentPrice}. ${seat} seats left  `
+        // / '$ ' + currentPrice + " " +seat + " seats left";
+    }
+}
+
+
+
+
+
+
+
+// var inputHappened = function(currentInput){
+//   console.log( currentInput );
+//   var output = "WOW TEMPERATURE";
+//   return output;
+// };
